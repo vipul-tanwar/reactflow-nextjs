0aa50a3028fb4589ae2dab893abd63ebea9fe890
Vipul Tanwar <thevipultanwar@gmail.com>
2023-03-03 02:43:26 +0530

    Add New Nodes

--- reactflow-nextjs/src/components/Flow/index.tsx
+++ reactflow-nextjs/src/components/Flow/index.tsx
@@ -27,20 +27,20 @@ const initialNodes: Node[] = [
   {
     id: '3',
     data: { label: 'Node 3' },
-    position: { x: 400, y: 100 },
+    position: { x: 300, y: 100 },
   },
+
   {
     id: '4',
     data: { label: 'Node 4' },
-    position: { x: 400, y: 200 },
-    type: 'custom',
-    className: styles.customNode,
+    position: { x: 600, y: 100 },
   },
 ];
 
 const initialEdges: Edge[] = [
   { id: 'e1-2', source: '1', target: '2' },
   { id: 'e1-3', source: '1', target: '3' },
+  { id: 'e1-4', source: '1', target: '4' },
 ];
 
 const nodeTypes = {
